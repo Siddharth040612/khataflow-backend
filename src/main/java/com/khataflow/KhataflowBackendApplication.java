e26df24fd1fa12eeb075f1a93e65f855c63180ef
siddharthjoshi <siddharthlearnings0406@gmail.com>
2026-04-05 00:01:08 +0530

    Initial backend setup (Spring Boot + basic API)

---
+++ src/main/java/com/khataflow/KhataflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.khataflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhataflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhataflowBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/khataflow/TestController.java
@@ -0,0 +1,18 @@
+package com.khataflow;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "KhataFlow Backend Running 🚀";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "API Working ✅";
+    }
+}
